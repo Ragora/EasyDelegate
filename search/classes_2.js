5d81987375d6857b8e716c3379a47ba16a718d54
Ragora <ragoradev@gmail.com>
2014-09-23 03:18:26 -0400

    Made small changes to the HTML docs

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['memberdelegate',['MemberDelegate',['../classEasyDelegate_1_1MemberDelegate.html',1,'EasyDelegate']]],
-  ['mycustomclass',['MyCustomClass',['../classMyCustomClass.html',1,'']]]
+  ['memberdelegate',['MemberDelegate',['../classEasyDelegate_1_1MemberDelegate.html',1,'EasyDelegate']]]
 ];
